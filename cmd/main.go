4e5f51c41dc6b6d17b92863c3ae0dc1f9c62b21f
Roman <romix0000@gmail.com>
2021-04-09 01:47:33 +0700

    Change Text

--- cmd/main.go
+++ cmd/main.go
@@ -10,7 +10,7 @@ func main() {
 	port := os.Getenv("PORT")
 
 	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi World!")
+		fmt.Fprintf(w, "Hi World! Let's GO")
 	})
 
 	http.ListenAndServe(":"+port, nil)
