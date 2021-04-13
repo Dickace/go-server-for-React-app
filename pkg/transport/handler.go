52db53910563790b4840cb74e62e896234089339
Roman <romix0000@gmail.com>
2021-04-13 21:50:22 +0700

    fix

--- pkg/transport/handler.go
+++ pkg/transport/handler.go
@@ -3,8 +3,10 @@ package transport
 import (
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	"io/ioutil"
 	"net/http"
 	"github.com/gorilla/schema"
+	"time"
 )
 
 func Router() http.Handler {
@@ -21,14 +23,28 @@ var decoder = schema.NewDecoder()
 
 func helloWorld(w http.ResponseWriter, r *http.Request) {
 	var valuteStruct ValuteStruct
-	var valute = r.URL.Query()
 	err := decoder.Decode(&valuteStruct, r.URL.Query())
 	if err != nil{
 		log.Fatal(err)
 	} else {
-		log.Println(valuteStruct)
-		log.Println(valute)
+		MakeRequest(valuteStruct.Valute)
 	}
+}
+func MakeRequest(nameValute string) {
+	var date = time.Now()
+	var prevDate = time.Date(date.Year(),date.Month(),date.Day() - 20, date.Hour(),date.Minute(),date.Second(),date.Nanosecond(),date.Location())
+	var url = "http://www.cbr.ru/scripts/XML_dynamic.asp?date_req1="+ prevDate.Format("2/1/2006") + "&date_req2=" + date.Format("2/1/2006") + "&VAL_NM_RQ="+ nameValute
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
 }
 
 func logMiddleware(h http.Handler) http.Handler {
